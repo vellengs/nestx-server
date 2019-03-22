fe1ffdbd5b01ced43385b4e6aa4195ad9a6b70e1
vellengs <vellengs@qq.com>
2019-03-22 16:31:45 +0800

    upgrade

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { ApplicationModule } from './server.module';
 import { ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './swagger';
 import * as compression from 'compression';
 import * as rateLimit from 'express-rate-limit';
 import * as helmet from 'helmet';
+import { AppModule } from 'app.module';
 // import { Utils } from './utils/utils';
 
 async function bootstrap() {
   // const httpsOptions = Utils.getKeyAndCert();
   const app = await NestFactory.create(
-    ApplicationModule
+    AppModule
   );
   app.use(rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100 // limit each IP to 100 requests per windowMs
   }));
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api'); // TODO 
   setupSwagger(app);
   app.enableCors();
   app.use(helmet());
